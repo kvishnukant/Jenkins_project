c9091524a0b7c904c1dab16eb22212b689115f88
Vishnu <Vishnu@gmail.com>
2015-01-05 18:41:14 +0530

    third commit

--- src/test/java/net/viralpatel/contact/controller/ContactControllerTest.java
+++ src/test/java/net/viralpatel/contact/controller/ContactControllerTest.java
@@ -1,10 +1,39 @@
 package net.viralpatel.contact.controller;
 
 import static org.junit.Assert.*;
+import net.viralpatel.contact.service.ContactService;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+
+@Controller
+@RunWith(MockitoJUnitRunner.class)
 public class ContactControllerTest {
+	
+	@Mock
+	ContactService contactService;
+	
+	@InjectMocks
+	ContactController contactController;
+	
+	MockMvc mockMvc;
+	
+	
+	@Before
+	public void setUp()
+	{
+		 MockitoAnnotations.initMocks(this);
+		    this.mockMvc = MockMvcBuilders.standaloneSetup(contactController).build();
+	}
 
 	@Test
 	public void test() {
